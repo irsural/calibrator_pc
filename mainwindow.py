4f6eea2e30ab89ccf17c48f9ca5ef58e6cfc8ddd
feelinglight <kolpacc123@mail.ru>
2019-12-23 16:56:14 +0500

    Теперь можно задавать амплитуду

--- mainwindow.py
+++ mainwindow.py
@@ -13,24 +13,52 @@ class MainWindow(QMainWindow):
         self.show()
 
         # self.clb_driver = ctypes.cdll.clb_driver_dll
-        self.clb_driver = ctypes.CDLL("C:\\Users\\503\\Desktop\\Qt Projects\\clb_driver_dll\\"
-                                      "build-clb_driver_dll-Desktop_Qt_5_12_2_MSVC2017_32bit-Release\\release\\"
-                                      "clb_driver_dll.dll")
-        self.clb_driver_get_name = self.clb_driver.usb_info_get_devices
-        # Возвращает список калибраторов, разделенных ';'
-        self.clb_driver_get_name.restype = ctypes.c_wchar_p
+        self.clb_driver = self.set_up_clb_driver_library("C:\\Users\\503\\Desktop\\Qt Projects\\clb_driver_dll\\"
+                                                         "build-clb_driver_dll-Desktop_Qt_5_12_2_MSVC2017_32bit-Release"
+                                                         "\\release\\clb_driver_dll.dll")
+
+        # Обязательно перед любыми действиями с clb_driver_dll
+        self.clb_driver.usb_init()
         print(self.clb_driver.version())
 
         self.usb_check_timer = QTimer()
         self.usb_check_timer.timeout.connect(self.update_usb)
         self.usb_check_timer.start(10)
 
+        self.connect_signals()
+
+    @staticmethod
+    def set_up_clb_driver_library(a_full_path):
+        clb_driver_lib = ctypes.CDLL(a_full_path)
+
+        # Возвращает список калибраторов, разделенных ';'
+        clb_driver_lib.get_usb_devices.restype = ctypes.c_wchar_p
+
+        clb_driver_lib.connect_usb.argtypes = [ctypes.c_wchar_p]
+        clb_driver_lib.set_amplitude.argtypes = [ctypes.c_double]
+
+        return clb_driver_lib
+
+    def connect_signals(self):
+        self.ui.clb_list_combobox.currentTextChanged.connect(self.connect_to_clb)
+        self.ui.amplitude_spinbox.valueChanged.connect(self.set_amplitude)
+
     def update_usb(self):
-        self.clb_driver.usb_info_tick()
-        if self.clb_driver.usb_info_changed():
+        self.clb_driver.usb_tick()
+        if self.clb_driver.usb_devices_changed():
             self.ui.clb_list_combobox.clear()
-            
-            clb_names_list: str = self.clb_driver_get_name()
+
+            clb_names_list: str = self.clb_driver.get_usb_devices()
             for clb_name in clb_names_list.split(';'):
                 if clb_name:
                     self.ui.clb_list_combobox.addItem(clb_name)
+
+    def connect_to_clb(self, a_clb_name):
+        if a_clb_name:
+            print(self.clb_driver.connect_usb(a_clb_name))
+        else:
+            self.clb_driver.disconnect_usb()
+
+    def set_amplitude(self):
+        amplitude = self.ui.amplitude_spinbox.value()
+        self.clb_driver.set_amplitude(amplitude)
--- ui/py/mainform.py
+++ ui/py/mainform.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(403, 221)
+        MainWindow.resize(212, 123)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -26,10 +26,23 @@ class Ui_MainWindow(object):
         self.clb_list_combobox.setFont(font)
         self.clb_list_combobox.setObjectName("clb_list_combobox")
         self.verticalLayout.addWidget(self.clb_list_combobox)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.amplitude_label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.amplitude_label.setFont(font)
+        self.amplitude_label.setObjectName("amplitude_label")
+        self.horizontalLayout.addWidget(self.amplitude_label)
+        self.amplitude_spinbox = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.amplitude_spinbox.setMaximum(600.0)
+        self.amplitude_spinbox.setObjectName("amplitude_spinbox")
+        self.horizontalLayout.addWidget(self.amplitude_spinbox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
         self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 403, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 212, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -42,3 +55,4 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.amplitude_label.setText(_translate("MainWindow", "Амплитуда"))
