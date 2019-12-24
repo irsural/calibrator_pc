6094d03422e6941a4052a032fc5e9965b65f46aa
feelinglight <kolpacc123@mail.ru>
2019-12-24 11:52:32 +0500

    Добавил задание режима калибратора и вывод статуса USB

--- calibrator_constants.py
+++ calibrator_constants.py
@@ -9,9 +9,24 @@ class UsbState(enum.IntEnum):
     ERROR = 4
 
 
-class DcPolatiry(enum.IntEnum):
+int_to_usb_status = {
+    UsbState.DISABLED: "Отключено",
+    UsbState.BUSY: "Подключение...",
+    UsbState.CONNECTED: "Подключено",
+    UsbState.ERROR: "Ошибка",
+    UsbState.NOT_SUPPORTED: "",
+}
+
+
+class Polatiry(enum.IntEnum):
     POS = 0
     NEG = 1
+
+
+int_to_polarity = {
+    Polatiry.POS: "+",
+    Polatiry.NEG: "-"
+}
 
 
 class SignalType(enum.IntEnum):
@@ -21,13 +36,27 @@ class SignalType(enum.IntEnum):
     DCV = 3
 
 
+class Mode(enum.IntEnum):
+    SOURCE = 0
+    FIXED_RANGE = 1
+    DETUNING = 2
+
+
+int_to_mode = {
+    Mode.SOURCE: "Источник",
+    Mode.FIXED_RANGE: "Фиксированный",
+    Mode.DETUNING: "Расстройка",
+}
+
+
 class ClbParams:
     def __init__(self):
         self.amplitude = 0
-        self.frequency = 40
+        self.frequency = 0
         self.signal_type = SignalType.ACI
-        self.dc_polarity = DcPolatiry.POS
+        self.dc_polarity = Polatiry.POS
         self.signal_on = False
+        self.mode = Mode.SOURCE
 
     def sync_parameter(self, param_name: str, value):
         variable = getattr(self, param_name)
--- mainwindow.py
+++ mainwindow.py
@@ -36,6 +36,13 @@ class MainWindow(QMainWindow):
             clb.SignalType.DCI: self.ui.dci_radio,
             clb.SignalType.DCV: self.ui.dcv_radio,
         }
+        self.int_to_mode = {
+            clb.Mode.SOURCE: self.ui.source_mode_radio,
+            clb.Mode.FIXED_RANGE: self.ui.fixed_mode_radio,
+            clb.Mode.DETUNING: self.ui.detuning_radio,
+        }
+        self.usb_status = clb.UsbState.DISABLED
+        self.block_signals = False
 
     @staticmethod
     def set_up_clb_driver_library(a_full_path):
@@ -61,6 +68,12 @@ class MainWindow(QMainWindow):
         clb_driver_lib.signal_enable.argtypes = [ctypes.c_int]
         clb_driver_lib.enabled.restype = ctypes.c_int
 
+        clb_driver_lib.get_usb_status.restype = ctypes.c_int
+        clb_driver_lib.is_connected.restype = ctypes.c_int
+
+        clb_driver_lib.set_mode.argtypes = [ctypes.c_int]
+        clb_driver_lib.get_mode.restype = ctypes.c_int
+
         return clb_driver_lib
 
     def connect_signals(self):
@@ -75,8 +88,15 @@ class MainWindow(QMainWindow):
         self.ui.dci_radio.toggled.connect(self.dci_radio_checked)
         self.ui.dcv_radio.toggled.connect(self.dcv_radio_checked)
 
+        self.ui.source_mode_radio.toggled.connect(self.source_radio_checked)
+        self.ui.fixed_mode_radio.toggled.connect(self.fixed_radio_checked)
+        self.ui.detuning_radio.toggled.connect(self.detuning_radio_checked)
+
+        self.ui.enable_button.clicked.connect(self.signal_enable)
+
     def update_usb(self):
         self.clb_driver.usb_tick()
+        self.clb_driver.clb_tick()
         if self.clb_driver.usb_devices_changed():
             self.ui.clb_list_combobox.clear()
 
@@ -84,6 +104,11 @@ class MainWindow(QMainWindow):
             for clb_name in clb_names_list.split(';'):
                 if clb_name:
                     self.ui.clb_list_combobox.addItem(clb_name)
+
+        usb_status = self.clb_driver.get_usb_status()
+        if self.usb_status != usb_status:
+            self.usb_status = usb_status
+            self.ui.usb_state_label.setText(clb.int_to_usb_status[usb_status])
 
         self.sync_clb_parameters()
 
@@ -94,19 +119,22 @@ class MainWindow(QMainWindow):
             self.clb_driver.disconnect_usb()
 
     def sync_clb_parameters(self):
+        self.block_signals = True
+
         if self.clb_params.sync_parameter("amplitude", self.clb_driver.get_amplitude()):
             self.ui.amplitude_spinbox.setValue(self.clb_params.amplitude)
         if self.clb_params.sync_parameter("frequency", self.clb_driver.get_frequency()):
             self.ui.frequency_spinbox.setValue(self.clb_params.frequency)
         if self.clb_params.sync_parameter("signal_type", self.clb_driver.get_signal_type()):
             self.int_to_signal_type[self.clb_params.signal_type].setChecked(True)
+
         if self.clb_params.sync_parameter("dc_polarity", self.clb_driver.get_polarity()):
-            if self.clb_params.dc_polarity == clb.DcPolatiry.POS:
+            self.ui.polarity_button.setText(clb.int_to_polarity[self.clb_params.dc_polarity])
+            if self.clb_params.dc_polarity == clb.Polatiry.POS:
                 self.ui.polarity_button.setChecked(False)
-                self.ui.polarity_button.setText("+")
             else:
                 self.ui.polarity_button.setChecked(True)
-                self.ui.polarity_button.setText("-")
+
         if self.clb_params.sync_parameter("signal_on", self.clb_driver.enabled()):
             if self.clb_params.signal_on:
                 self.ui.enable_button.setChecked(True)
@@ -115,35 +143,58 @@ class MainWindow(QMainWindow):
                 self.ui.enable_button.setChecked(False)
                 self.ui.enable_button.setText("Enable")
 
+        if self.clb_params.sync_parameter("mode", self.clb_driver.get_mode()):
+            self.int_to_mode[self.clb_params.mode].setChecked(True)
+
+        self.block_signals = False
+
     def set_amplitude(self):
-        amplitude = self.ui.amplitude_spinbox.value()
-        self.clb_driver.set_amplitude(amplitude)
+        if not self.block_signals:
+            amplitude = self.ui.amplitude_spinbox.value()
+            self.clb_driver.set_amplitude(amplitude)
 
     def set_frequency(self):
-        frequency = self.ui.frequency_spinbox.value()
-        self.clb_driver.set_frequency(frequency)
+        if not self.block_signals:
+            frequency = self.ui.frequency_spinbox.value()
+            self.clb_driver.set_frequency(frequency)
 
     def polarity_button_clicked(self, a_checked):
-        if a_checked:
-            self.clb_driver.set_polarity(clb.DcPolatiry.NEG)
-            self.ui.polarity_button.setText("-")
-        else:
-            self.clb_driver.set_polarity(clb.DcPolatiry.POS)
-            self.ui.polarity_button.setText("+")
+        if not self.block_signals:
+            if a_checked:
+                self.clb_driver.set_polarity(clb.Polatiry.NEG)
+                self.ui.polarity_button.setText(clb.int_to_polarity[clb.Polatiry.NEG])
+            else:
+                self.clb_driver.set_polarity(clb.Polatiry.POS)
+                self.ui.polarity_button.setText(clb.int_to_polarity[clb.Polatiry.POS])
 
     def aci_radio_checked(self):
-        self.clb_driver.set_signal_type(clb.SignalType.ACI)
-        # pass
+        if not self.block_signals:
+            self.clb_driver.set_signal_type(clb.SignalType.ACI)
 
     def acv_radio_checked(self):
-        self.clb_driver.set_signal_type(clb.SignalType.ACV)
-        # pass
+        if not self.block_signals:
+            self.clb_driver.set_signal_type(clb.SignalType.ACV)
 
     def dci_radio_checked(self):
-        self.clb_driver.set_signal_type(clb.SignalType.DCI)
-        # pass
+        if not self.block_signals:
+            self.clb_driver.set_signal_type(clb.SignalType.DCI)
 
     def dcv_radio_checked(self):
-        self.clb_driver.set_signal_type(clb.SignalType.DCV)
-        # pass
+        if not self.block_signals:
+            self.clb_driver.set_signal_type(clb.SignalType.DCV)
 
+    def source_radio_checked(self):
+        if not self.block_signals:
+            self.clb_driver.set_mode(clb.Mode.SOURCE)
+
+    def fixed_radio_checked(self):
+        if not self.block_signals:
+            self.clb_driver.set_mode(clb.Mode.FIXED_RANGE)
+
+    def detuning_radio_checked(self):
+        if not self.block_signals:
+            self.clb_driver.set_mode(clb.Mode.DETUNING)
+
+    def signal_enable(self, a_enable):
+        if not self.block_signals:
+            self.clb_driver.signal_enable(a_enable)
