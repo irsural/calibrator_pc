288afc782e77527bf282994b24300f03a9f1e261
feelinglight <kolpacc123@mail.ru>
2020-02-27 16:23:30 +0500

    Добавил возможность копировать дефолтные метки в буфер обмена

--- create_protocol_dialog.py
+++ create_protocol_dialog.py
@@ -1,4 +1,5 @@
 from sqlite3 import Connection
+from re import compile as re_compile
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -9,6 +10,8 @@ from marks_widget import MarksWidget
 
 
 class CreateProtocolDialog(QtWidgets.QDialog):
+    GET_MARK_RE = re_compile(r"%.*__")
+
     def __init__(self, a_settings: Settings, a_measure_id: int, a_db_connection: Connection,
                  a_db_tables: MeasureTables, a_parent=None):
         super().__init__(a_parent)
@@ -28,11 +31,36 @@ class CreateProtocolDialog(QtWidgets.QDialog):
                                         a_measure_id=self.measure_config.id, a_parent=self)
         self.ui.marks_widget_layout.addWidget(self.marks_widget)
 
+        self.create_label_context_menu()
         self.set_up_params_to_ui(points)
 
         self.ui.accept_button.clicked.connect(self.save_pressed)
         self.ui.reject_button.clicked.connect(self.reject)
 
+    # noinspection DuplicatedCode
+    def create_label_context_menu(self):
+        self.ui.user_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.organisation_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.date_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.name_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.serial_number_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.signal_type_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.device_creator_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.system_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.class_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.etalon_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.owner_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+        self.ui.comment_label.customContextMenuRequested.connect(self.show_label_custom_menu)
+
+    def show_label_custom_menu(self, a_position: QtCore.QPoint):
+        label = self.sender()
+        assert isinstance(label, QtWidgets.QLabel), "show_label_custom_menu must be connected to QLabel!"
+        menu = QtWidgets.QMenu(label)
+        copy_mark_act = menu.addAction("Копировать метку")
+        copy_mark_act.triggered.connect(self.copy_label_mark)
+        menu.popup(label.mapToGlobal(a_position))
+
+    # noinspection DuplicatedCode
     def set_up_params_to_ui(self, a_points):
         self.ui.user_name_edit.setText(self.measure_config.user)
         self.ui.device_name_edit.setText(self.measure_config.device_name)
@@ -51,11 +79,21 @@ class CreateProtocolDialog(QtWidgets.QDialog):
 
         # Восстановить точки
 
+    def copy_label_mark(self):
+        # noinspection PyTypeChecker
+        label: QtWidgets.QLabel = self.sender().parent().parent()
+        assert isinstance(label, QtWidgets.QLabel), "This slot must be called by Qmenu of QLabel!!"
+        mark_match = self.GET_MARK_RE.search(label.text())
+        assert mark_match is not None, "Label must contain mark in format %*__ !!"
+        mark_text = mark_match.group(0)
+        QtWidgets.QApplication.clipboard().setText(mark_text)
+
     def save_pressed(self):
         if self.marks_widget.save():
             self.save()
             self.close()
 
+    # noinspection DuplicatedCode
     def save(self):
         self.measure_config.user = self.ui.user_name_edit.text()
         self.measure_config.device_name = self.ui.device_name_edit.text()
